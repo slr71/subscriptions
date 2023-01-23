872e4c28adc121e2ffd459fade9ada7c84111fea
John Wregglesworth <wregglej@cyverse.org>
2023-01-23 11:33:54 -0700

    The rest of the work to add  AddAddonHandler

---
+++ app/addons.go
@@ -0,0 +1,88 @@
+package app
+
+import (
+	"context"
+
+	"errors"
+
+	serrors "github.com/cyverse-de/subscriptions/errors"
+
+	qmsinit "github.com/cyverse-de/go-mod/pbinit/qms"
+	"github.com/cyverse-de/p/go/qms"
+	"github.com/cyverse-de/subscriptions/db"
+)
+
+func (a *App) AddAddonHandler(subject, reply string, request *qms.AddAddonRequest) {
+	var err error
+
+	log := log.WithField("context", "adding new available addon")
+
+	response := qmsinit.NewAddonResponse()
+
+	ctx, span := qmsinit.InitAddAddonRequest(request, subject)
+	defer span.End()
+
+	sendError := func(ctx context.Context, response *qms.AddonResponse, err error) {
+		log.Error(err)
+		response.Error = serrors.NatsError(ctx, err)
+		if err = a.client.Respond(ctx, reply, response); err != nil {
+			log.Error(err)
+		}
+	}
+
+	d := db.New(a.db)
+
+	reqAddon := request.Addon
+
+	if reqAddon.Name == "" {
+		sendError(ctx, response, errors.New("name must be set"))
+		return
+	}
+
+	if reqAddon.Description == "" {
+		sendError(ctx, response, errors.New("descriptions must be set"))
+		return
+	}
+
+	if reqAddon.DefaultAmount <= 0.0 {
+		sendError(ctx, response, errors.New("default_amount must be greater than 0.0"))
+		return
+	}
+
+	if reqAddon.ResourceType.Name == "" && reqAddon.ResourceType.Uuid == "" {
+		sendError(ctx, response, errors.New("resource_type.name or resource_type.uuid must be set"))
+		return
+	}
+
+	var lookupRT *db.ResourceType
+
+	if reqAddon.ResourceType.Name != "" && reqAddon.ResourceType.Uuid == "" {
+		lookupRT, err = d.GetResourceTypeByName(ctx, reqAddon.ResourceType.Name)
+		if err != nil {
+			sendError(ctx, response, err)
+			return
+		}
+	} else {
+		lookupRT, err = d.GetResourceType(ctx, reqAddon.ResourceType.Uuid)
+		if err != nil {
+			sendError(ctx, response, err)
+			return
+		}
+	}
+
+	newAddon := db.NewAddonFromQMS(request.Addon)
+	newAddon.ResourceType = *lookupRT
+
+	newID, err := d.AddAddon(ctx, newAddon)
+	if err != nil {
+		sendError(ctx, response, err)
+		return
+	}
+
+	response.Addon = newAddon.ToQMSType()
+	response.Addon.Uuid = newID
+
+	if err = a.client.Respond(ctx, reply, response); err != nil {
+		log.Error(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/cyverse-de/go-mod/gotelnats v0.0.11
 	github.com/cyverse-de/go-mod/logging v0.0.2
 	github.com/cyverse-de/go-mod/otelutils v0.0.3
-	github.com/cyverse-de/go-mod/pbinit v0.1.0
+	github.com/cyverse-de/go-mod/pbinit v0.1.4
 	github.com/cyverse-de/go-mod/protobufjson v0.0.3
 	github.com/cyverse-de/go-mod/subjects v0.1.0
 	github.com/cyverse-de/p/go/qms v0.1.1
